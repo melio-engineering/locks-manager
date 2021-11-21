6f223b78ade8d7ab071aaf3a1d2b13c4ad1201a2
Yariv Kohn <yariv@meliopayments.com>
2021-11-21 22:19:13 +0200

    Fix condition

--- src/dynamodb/conditions/insert.ts
+++ src/dynamodb/conditions/insert.ts
@@ -6,7 +6,7 @@ export const FORMAT_TIMESTAMP_IN_SECONDS = 'X';
 export const getInsertCondition = (id: string) => {
   const now = parseInt(moment().format(FORMAT_TIMESTAMP_IN_SECONDS), 10);
   return new Condition({
-    ConditionExpression: '#key <> :key OR (#key = :key AND #expire > :now)',
+    ConditionExpression: '#key <> :key OR (#key = :key AND #expire < :now)',
     ExpressionAttributeNames: {
       '#expire': 'timestamp',
       '#key': 'id',
