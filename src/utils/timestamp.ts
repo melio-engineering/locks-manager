bf42b5eb8932d00f13e8fc32c1b8f20a235aa93d
Yariv Kohn <yariv@meliopayments.com>
2021-11-24 15:26:37 +0200

    MEl4-890 refactor cleaner code

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -5,16 +5,18 @@ const FORMAT_TIMESTAMP_IN_SECONDS = 'X';
 export class Timestamp {
   static get = (): number =>
     parseInt(
-      moment()
-        .utc()
+      Timestamp.getMomentUtcTimestamp()
         .format(FORMAT_TIMESTAMP_IN_SECONDS),
       10);
 
   static getLockTtl = (lockHoldTime: number): number =>
     parseInt(
-      moment()
-        .utc()
+      Timestamp.getMomentUtcTimestamp()
         .add(lockHoldTime, 'seconds')
         .format(FORMAT_TIMESTAMP_IN_SECONDS),
       10);
+
+  private static getMomentUtcTimestamp() {
+    return moment().utc();
+  }
 }
